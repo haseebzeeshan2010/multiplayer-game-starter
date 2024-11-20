93362d22d99179918c5113d708b83a408f1687c6
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-11-20 20:02:07 +0000

    Remove enemy function and install template

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public')) //Makes any file in this directory available to public (js folder)
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //Sends file of index.html if visiting url homepage
   res.sendFile(__dirname + '/index.html')
 })
 
