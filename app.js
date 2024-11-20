18c38900a5d08a746a949c676575987d0cee8026
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-11-20 20:32:36 +0000

    Do initial socket setup

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 app.use(express.static('public')) //Makes any file in this directory available to public (js folder)
 
 app.get('/', (req, res) => { //Sends file of index.html if visiting url homepage
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
